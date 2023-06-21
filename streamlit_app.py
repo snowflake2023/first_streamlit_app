87f8ef3c09c52327dabbc2492197fd0d51dcbf1c
snowflake2023 <137274231+snowflake2023@users.noreply.github.com>
2023-06-21 15:03:46 +0530

    added breakfast menu header and 3 items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
-streamlit.title("New Streamlit")
+streamlit.title("My Parents New Healthy Diner")
+
+streamlit.header("Breakfast Menu")
+streamlit.header("Omega 3 & Blueberry Oatmeal")
+streamlit.header("Kale, Spinach & Rocket Smoothie")
+streamlit.header("Hard-Boiled Free-Range Egg")
