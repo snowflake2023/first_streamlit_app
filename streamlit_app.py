42835fbbcef04e2907337993da17bbe1288841a7
snowflake2023 <137274231+snowflake2023@users.noreply.github.com>
2023-06-21 16:39:41 +0530

    update default values

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,8 +11,8 @@ streamlit.header("Avacado Toast")
 streamlit.header("Build your Own Fruit Smoothie")
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-default_fruits = list(my_fruit_list.index).sample(2)
+default_fruits = list(my_fruit_list.index)[:2]
 streamlit.write(default_fruits,' ',type(default_fruits))
-streamlit.multiselect("Pick some fruits:",options=list(my_fruit_list.index),default=list(default_fruits))
+streamlit.multiselect("Pick some fruits:",options=list(my_fruit_list.index),default=default_fruits)
 streamlit.dataframe(my_fruit_list)
 
