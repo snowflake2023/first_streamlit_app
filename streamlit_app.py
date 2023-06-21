0e3165b6887c54e011cb2a17cf93de1041ced03b
snowflake2023 <137274231+snowflake2023@users.noreply.github.com>
2023-06-21 16:12:07 +0530

    updated s3 file path

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,5 @@ streamlit.header("Hard-Boiled Free-Range Egg")
 streamlit.header("Avacado Toast")
 
 streamlit.header("Build your Own Fruit Smoothie")
-
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
