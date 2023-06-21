c9514e2776822dfaf262a5e14a726a2b08690a49
snowflake2023 <137274231+snowflake2023@users.noreply.github.com>
2023-06-21 16:32:05 +0530

    added multiselct options

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,6 @@ streamlit.header("Avacado Toast")
 streamlit.header("Build your Own Fruit Smoothie")
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruite_list = my_fruit_list.set_index('Fruit')
-streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
+streamlit.multiselect("Pick some fruits:",options=list(my_fruit_list.index),default=["Avocado","Strawberries"])
 streamlit.dataframe(my_fruit_list)
 
